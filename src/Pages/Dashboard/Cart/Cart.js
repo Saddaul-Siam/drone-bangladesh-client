1fcf8d743b6af778f1ad3e699096d016e888ed2f
saddaul-siam <saddadul.siam@gmail.com>
2021-12-16 20:28:58 +0600

    added cart action

--- src/Pages/Dashboard/Cart/Cart.js
+++ src/Pages/Dashboard/Cart/Cart.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Box, styled } from "@mui/system";
-import { Button, Container, Grid } from "@mui/material";
+import { Button, Container, Grid, Typography } from "@mui/material";
 import {
   clearTheCart,
   getStoredCart,
   removeFromDb,
 } from "../../../utilities/fakedb";
 import { Link } from "react-router-dom";
+import CartDetails from "./CartDetails";
 
 const Root = styled("div")`
   table {
@@ -29,7 +30,6 @@ const Root = styled("div")`
 export default function Cart() {
   const [products, setProducts] = useState([]);
   const [carts, setCarts] = useState([]);
-  console.log(carts);
   useEffect(() => {
     fetch("http://localhost:5000/products")
       .then((res) => res.json())
@@ -56,6 +56,11 @@ export default function Cart() {
     const total = parseInt(product.price) * parseInt(product.quantity);
     product.total = total;
   }
+
+  let totalShoppingCost = 0;
+  for (const product of carts) {
+    totalShoppingCost = totalShoppingCost + product.total;
+  }
   const handleClearCart = () => {
     clearTheCart();
     window.location.reload();
@@ -65,61 +70,102 @@ export default function Cart() {
     window.location.reload();
   };
   return (
-    <Container>
-      <Root>
-        <table style={{ minWidth: 500 }} aria-label="custom pagination table">
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th style={{ width: 400 }}>Product</th>
-              <th style={{ width: 199 }}>Price</th>
-              <th style={{ width: 400 }}>Quantity</th>
-              <th style={{ width: 199 }}>Total</th>
-              <th style={{ width: 199 }}>Remove</th>
-            </tr>
-          </thead>
-          <tbody>
-            {carts.map((cart) => (
-              <tr key={cart._id}>
-                <td>
-                  <img height="120px" width="100px" src={cart.img} alt="" />
-                </td>
-                <td style={{ width: 400 }}>{cart.name}</td>
-                <td style={{ width: 199 }}>{cart.price}</td>
-                <td style={{ width: 400}}>{cart.quantity}</td>
-                <td style={{ width: 199 }}>{cart.total}</td>
-                <td style={{ width: 199 }}>
-                  <Button
-                    variant="text"
-                    onClick={() => handleRemoveCart(cart._id)}
-                  >
-                    X
-                  </Button>
-                </td>
+    <>
+      <Container>
+        <Root>
+          <table style={{ minWidth: 500 }} aria-label="custom pagination table">
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th style={{ width: 400 }}>Product</th>
+                <th style={{ width: 199 }}>Price</th>
+                <th style={{ width: 400 }}>Quantity</th>
+                <th style={{ width: 199 }}>Total</th>
+                <th style={{ width: 199 }}>Remove</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </Root>
-      <Box sx={{ pt: 3 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={8}>
-            <Link to="/" style={{ textDecoration: "none", color: "black" }}>
-              <Button variant="contained">CONTINUE SHOPPING </Button>
-            </Link>
+            </thead>
+            <tbody>
+              {carts.map((cart) => (
+                <tr key={cart._id}>
+                  <td>
+                    <img height="120px" width="100px" src={cart.img} alt="" />
+                  </td>
+                  <td style={{ width: 400 }}>{cart.name}</td>
+                  <td style={{ width: 199 }}>{cart.price}</td>
+                  <td style={{ width: 400 }}>{cart.quantity}</td>
+                  <td style={{ width: 199 }}>{cart.total}</td>
+                  <td style={{ width: 199 }}>
+                    <Button
+                      variant="text"
+                      onClick={() => handleRemoveCart(cart._id)}
+                    >
+                      X
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </Root>
+        <Box sx={{ pt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+                <Button variant="contained">CONTINUE SHOPPING </Button>
+              </Link>
+            </Grid>
+            <Grid
+              item
+              xs={4}
+              sx={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <Button variant="contained">UPDATE CART</Button>
+              <Button variant="contained" onClick={handleClearCart}>
+                CLEAR CART
+              </Button>
+            </Grid>
           </Grid>
-          <Grid
-            item
-            xs={4}
-            sx={{ display: "flex", justifyContent: "space-between" }}
+        </Box>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Box
+            sx={{
+              width: "500px",
+              background: "#F5F5F5",
+              mt: 5,
+              p: 4,
+              pr: 6,
+            }}
           >
-            <Button variant="contained">UPDATE CART</Button>
-            <Button variant="contained" onClick={handleClearCart}>
-              CLEAR CART
+            <Typography sx={{ fontWeight: "bold", fontSize: 24, pb: 3 }}>
+              Cart Totals
+            </Typography>
+            <Grid container spacing={2} sx={{ mx: 0 }}>
+              <Grid item xs={6} sx={{ border: 1, p: 1 }}>
+                <Typography variant="h5">Subtotal</Typography>
+              </Grid>
+              <Grid item xs={6} sx={{ border: 1, borderLeft: 0, p: 1 }}>
+                <Typography variant="h5">$ {totalShoppingCost}</Typography>
+              </Grid>
+              <Grid item xs={6} sx={{ border: 1, borderTop: 0, p: 1 }}>
+                <Typography variant="h5">Total</Typography>
+              </Grid>
+              <Grid
+                item
+                xs={6}
+                sx={{ border: 1, borderTop: 0, borderLeft: 0, p: 1 }}
+              >
+                <Typography variant="h5">$ {totalShoppingCost}</Typography>
+              </Grid>
+            </Grid>
+            <Button variant="contained" sx={{ mt: 3, align: "left" }}>
+              Proceed to Checkout
             </Button>
-          </Grid>
-        </Grid>
-      </Box>
-    </Container>
+          </Box>
+        </Box>
+        <Box sx={{ display: "none" }}>
+          <CartDetails carts={carts} totalShoppingCost={totalShoppingCost} />
+        </Box>
+      </Container>
+    </>
   );
 }
---
+++ src/Pages/Dashboard/Cart/CartDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Information = ({ carts,totalShoppingCost }) => {
+  console.log(totalShoppingCost);
+  return (
+    <div>
+      <h2>This is information</h2>
+    </div>
+  );
+};
+
+export default Information;
